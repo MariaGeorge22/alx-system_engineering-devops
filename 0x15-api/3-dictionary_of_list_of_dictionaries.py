89fa636a00956216fd376349643e90a8cc2ffb3c
MariaGeorge22”git config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-05-21 14:06:13 -0700

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -31,12 +31,13 @@ def get_all_employees():
         )
         for todo in user_todos:
             dict[id].append(
-                {"task": todo.get("title"),
-                "completed": todo.get("completed"),
-                "username": username, }
+                    {"task": todo.get("title"),
+                    "completed": todo.get("completed"),
+                    "username": username, }
             )
     with open("todo_all_employees.json", 'w') as json_file:
         json.dump(dict, json_file)
 
-    if __name__ == "__main__":
-        get_all_employees()
+
+if __name__ == "__main__":
+    get_all_employees()
